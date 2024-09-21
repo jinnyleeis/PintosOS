231bca02c8220d41ff3d877e36d0cdfe2d4df5ac
jinnyleeis <wldls317@sogang.ac.kr>
2024-09-21 05:23:13 +0000

    [feat] enum에 추가 시스템콜 번호 추가,” -m “SYS_FIBONACCI, SYS_MAX_OF_FOUR_INT”에 해당하는 시스템 콜 번호 enum에 추가 + 헷갈리지 않도록, 주석으로 시스템콜 별 번호 설명

--- pintos/src/lib/syscall-nr.h
+++ pintos/src/lib/syscall-nr.h
@@ -5,19 +5,22 @@
 enum 
   {
     /* Projects 2 and later. */
-    SYS_HALT,                   /* Halt the operating system. */
-    SYS_EXIT,                   /* Terminate this process. */
-    SYS_EXEC,                   /* Start another process. */
-    SYS_WAIT,                   /* Wait for a child process to die. */
-    SYS_CREATE,                 /* Create a file. */
-    SYS_REMOVE,                 /* Delete a file. */
-    SYS_OPEN,                   /* Open a file. */
-    SYS_FILESIZE,               /* Obtain a file's size. */
-    SYS_READ,                   /* Read from a file. */
-    SYS_WRITE,                  /* Write to a file. */
-    SYS_SEEK,                   /* Change position in a file. */
-    SYS_TELL,                   /* Report current position in a file. */
-    SYS_CLOSE,                  /* Close a file. */
+    SYS_HALT,                   /* 0번 Halt the operating system. */
+    SYS_EXIT,                   /* 1번 Terminate this process. */
+    SYS_EXEC,                   /* 2번 Start another process. */
+    SYS_WAIT,                   /* 3번 Wait for a child process to die. */
+    SYS_CREATE,                 /* 4번 Create a file. */
+    SYS_REMOVE,                 /* 5번 Delete a file. */
+    SYS_OPEN,                   /* 6번 Open a file. */
+    SYS_FILESIZE,               /* 7번 Obtain a file's size. */
+    SYS_READ,                   /* 8번 Read from a file. */
+    SYS_WRITE,                  /* 9번 Write to a file. */
+    SYS_SEEK,                   /* 10번 Change position in a file. */
+    SYS_TELL,                   /* 11번 Report current position in a file. */
+    SYS_CLOSE,                  /* 12번 Close a file. */
+    // 추가적인 시스템 콜 !! 
+    SYS_FIBONACCI,              /* 13번 피보나치의 수 계산 */
+    SYS_MAX_OF_FOUR_INT,        /* 14번 4개의 수 중 가장 큰 것 리턴 */
 
     /* Project 3 and optionally project 4. */
     SYS_MMAP,                   /* Map a file into memory. */
